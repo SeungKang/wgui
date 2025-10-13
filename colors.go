c171f732d3c1e465fb07bd4931653a2e4af7590a
SeungKang <13smk@queensu.ca>
2025-10-12 20:46:12 -0400

    gui: changed app colors

--- colors.go
+++ colors.go
@@ -6,8 +6,8 @@ var (
 	WhiteColor     = color.NRGBA{A: 0xff, R: 255, G: 255, B: 255}
 	GreyColor      = color.NRGBA{A: 0xff, R: 75, G: 75, B: 75}
 	RedColor       = color.NRGBA{A: 0xff, R: 162, G: 0, B: 0}
-	BgColor        = color.NRGBA{A: 0xff, R: 30, G: 30, B: 30}
-	SidebarBg      = color.NRGBA{A: 0xff, R: 40, G: 40, B: 40}
+	SidebarBg      = color.NRGBA{A: 0xff, R: 30, G: 30, B: 30}
+	BgColor        = color.NRGBA{A: 0xff, R: 35, G: 35, B: 35}
 	SelectedBg     = color.NRGBA{A: 0xff, R: 70, G: 70, B: 90}
 	PurpleColor    = color.NRGBA{A: 0xff, R: 99, G: 96, B: 225}
 	GreenColor     = color.NRGBA{A: 0xff, R: 59, G: 125, B: 35}
